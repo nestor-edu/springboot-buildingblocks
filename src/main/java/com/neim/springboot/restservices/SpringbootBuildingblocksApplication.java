89e3ecbffcc0347fcc1ca7b71e422a2d73116fad
Nestor Eduardo <neim1996@hotmail.com>
2022-09-20 10:37:06 -0600

    i18n first commit

--- src/main/java/com/neim/springboot/restservices/SpringbootBuildingblocksApplication.java
+++ src/main/java/com/neim/springboot/restservices/SpringbootBuildingblocksApplication.java
@@ -2,6 +2,12 @@ package com.neim.springboot.restservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class SpringbootBuildingblocksApplication {
@@ -9,5 +15,16 @@ public class SpringbootBuildingblocksApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootBuildingblocksApplication.class, args);
 	}
-
+	@Bean
+	public LocaleResolver localeResolver() {
+		AcceptHeaderLocaleResolver headerLocaleResolver = new AcceptHeaderLocaleResolver();
+		headerLocaleResolver.setDefaultLocale(Locale.US);
+		return headerLocaleResolver;
+	}
+	@Bean
+	public ResourceBundleMessageSource messageSource() {
+		ResourceBundleMessageSource bundleMessageSource = new ResourceBundleMessageSource();
+		bundleMessageSource.setBasename("messages");
+		return bundleMessageSource;
+	}
 }
--- src/main/java/com/neim/springboot/restservices/controllers/HelloController.java
+++ src/main/java/com/neim/springboot/restservices/controllers/HelloController.java
@@ -1,10 +1,20 @@
 package com.neim.springboot.restservices.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController
 public class HelloController {
+
+    @Autowired
+    private ResourceBundleMessageSource messageSource;
+
     @GetMapping("/welcome")
     public String helloWorld() {
         return "Hello Nestor";
@@ -13,5 +23,15 @@ public class HelloController {
     @GetMapping("/user-bean")
     public UserDetails userDetails() {
         return new UserDetails("Nestor Mazariego", "neim1996@hotmail.com", "San Jose");
+    }
+
+    @GetMapping("/hello-int")
+    public String getMessagesI18nFormat(@RequestHeader(name = "Accept-Language", required = false) String locale) {
+        return messageSource.getMessage("label.hello", null, new Locale(locale));
+    }
+
+    @GetMapping("/hello-int2")
+    public String getMessagesI18nFormat2() {
+        return messageSource.getMessage("label.hello", null, LocaleContextHolder.getLocale());
     }
 }
